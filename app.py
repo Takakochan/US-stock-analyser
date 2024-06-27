6f9fefb6b3bd1f6b97440065e591a076477cd0c8
TKK <73033322+Takakochan@users.noreply.github.com>
2024-06-27 21:35:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with col1:
 
     for d in date_list2:
         paireddate_list.append((d))
-        tickersymbol2[d] = tickersymbol2[d].str.replace('%','').str.replace('−','-').astype(float)
+        tickersymbol2[d] = tickersymbol2[d].replace('%','').replace('−','-').astype(float)
         eps_list.append(float(tickersymbol2[d]))
     #eps_list2 = []
     #for p in eps_list:
