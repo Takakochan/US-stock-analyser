fc8d99ee7df982cbf1c8778fdab643981a5bc83b
TKK <73033322+Takakochan@users.noreply.github.com>
2025-04-25 15:06:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -155,10 +155,8 @@ with col1:
     except:
       pass
 with col2:
-  client = OpenAI(
-        # This is the default and can be omitted
-    api_key = st.secrets["OPENAI_API_KEY"]
-    client = OpenAI(api_key=api_key)
+api_key = st.secrets["OPENAI_API_KEY"]
+client = OpenAI(api_key=api_key)
 
   response = client.responses.create(
       model="gpt-3.5-turbo",
