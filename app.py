646c21783f530811565a336529488013dc58d915
TKK <73033322+Takakochan@users.noreply.github.com>
2025-05-06 21:59:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ with col2:
 
 with col3:
   st.write("competitor's growth")
-  responce2 = client.responses.create(model="gpt-3.5-turbo", input = "return only a list in python script, extracting only stock symbols from the text, (eg. AAPL, META, MSFT) "+ response.output_text)
+  responce2 = client.responses.create(model="gpt-3.5-turbo", input = "return only a list, extracting only stock symbols from the text, "+ response.output_text+"(eg. AAPL, META, MSFT) ")
   st.write(responce2.output_text)
   res = responce2.output_text
   res=res.split(', ')
